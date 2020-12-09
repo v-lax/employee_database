d989de3bcd5f1251abde962cad57bfcf55ddd9e3
valliappan Lakshmanan <valliappanlakshmanan@valliappans-MacBook-Air.local>
2020-12-08 23:40:53 -0500

    updated my javascript file with basic functinoality and framework for asking questions

---
+++ employee.js
@@ -0,0 +1,85 @@
+var mysql = require("mysql");
+var inq = require('inquirer');
+
+var connection = mysql.createConnection({
+    host:'localhose',
+    port:3306,
+    user:'root',
+    password:"",
+    database:"employee_DB"
+}); 
+
+connection.connect(function(err){
+    if(err)throw err;
+    init();
+})
+
+function init(){
+    inq.prompt({
+        name:'action',
+        type:'list',
+        message:'What would you like to do?',
+        choices:['Add','View','Update','Exit']
+    }).then(function(response){
+        if(response.action==='Add'){
+            addValue();
+        }else if(response.action==='View'){
+            viewValue();
+        }else if(response.action==='Update'){
+            updateValue();
+        }else{
+            connection.end();
+        }
+    })
+}
+
+function addValue(){
+    inq.prompt({
+            name:'addType',
+            type:'list',
+            message:"What would you like to add to the database?",
+            choices:['Employee','Department','Role']
+        }).then(function(response){
+            if(response.addType==='Employee'){
+                addEmployee()
+            }else if(response.addType==='Department'){
+                addDepartment()
+            }else if(response.addType==='Role'){
+                addRole()
+            }
+        })
+}
+
+function viewValue(){
+    inq.prompt({
+        name:'viewType',
+        type:'list',
+        message:'What would you like to view',
+        choices:['View all departments','View all employees','View all roles','View all employees by manager']
+    }).then(function(response){
+        if(response.viewType==='View all departments'){
+            viewDepartment()
+        }else if(response.viewType==='View all employees'){
+            viewEmployees()
+        }else if(response.viewType==='View all roles'){
+            viewRoles()
+        }else if(response.viewType==='View all employees by manager'){
+            viewEmployeesByMananger()
+        }
+    })
+}
+
+function updateValue(){
+    inq.prompt({
+        name:'updateType',
+        type:'list',
+        message:'What you like to update?',
+        choices:['Update employee roles','Update employee managers']
+    }).then(function(response){
+        if(response.updateType==='Update employee roles'){
+            updateRoles()
+        }else if(response.updateType==='Update employee managers'){
+            updateManagers()
+        }
+    })
+}
